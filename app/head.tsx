df5ff149f345afbccd499de3a7234d7617d20676
tradepairenpai-cloud <tradepairenpai@gmail.com>
2026-09-15 22:39:53 +0700

    Use project-relative PWA links

--- app/head.tsx
+++ app/head.tsx
@@ -5,9 +5,9 @@ export default function Head() {
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      <link rel="manifest" href="/app.webmanifest" />
-      <link rel="apple-touch-icon" href="/favicon.svg" />
-      <script src="/register-sw.js" defer />
+      <link rel="manifest" href="app.webmanifest" />
+      <link rel="apple-touch-icon" href="favicon.svg" />
+      <script src="register-sw.js" defer />
     </>
   );
 }
